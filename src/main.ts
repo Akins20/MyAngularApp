c41bca63231aad1105be56a9de8823cd2928251f
Akins20 <ogunbiye@gmail.com>
2023-01-25 19:00:47 +0100

    Init app

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+var i = 1;
+function lod() {
+  if (i == 0) {
+    i = 1;
+    var elem: any = document.getElementById('mySkill');
+    var width: number = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
